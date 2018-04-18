274e9276be1f91edf1fe95589e3ae870140132c1
Alexandre Agustini <alexandre,agustini@pucrs.br>
2018-04-18 06:24:36 -0700

    Uso do EFCore, criacao do BD

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -1,31 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+
 using TodoApi.Models;
 
 namespace TodoApi
 {
     public class Startup
     {
-       // public Startup(IConfiguration configuration)
-        //{
-        //    Configuration = configuration;
-        //}
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-       // public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+            //services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+
+            services.AddDbContext<TodoContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("TodoConnection")));
+               
+            
+            
             services.AddMvc();
         }
 
